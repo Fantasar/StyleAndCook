da4b7e3ca99ecf0817fe44f85e5c39686600e22d
Philippe Lapique <lapique.philippe@gmail.com>
2025-06-21 11:45:36 +0200

    move le dossier effet dans entities

--- obj/Debug/net9.0/StyleAndCook.AssemblyInfo.cs
+++ obj/Debug/net9.0/StyleAndCook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StyleAndCook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc6695088b2b7fec3b41521a436610e7049de176")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8690cabea3f8afc0e2b2dcc81f13b5aaaab2c376")]
 [assembly: System.Reflection.AssemblyProductAttribute("StyleAndCook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StyleAndCook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
